e06f31933b4f35dc2909df0e3913fea41d71628b
Raja Mathiyalagan <87691278+Raja0514@users.noreply.github.com>
2024-12-21 22:23:32 +0400

    class

---
+++ 05_module_wrapper_function/app.js
@@ -0,0 +1,14 @@
+
+    console.log(__dirname);
+    console.log(__filename);
+    console.log(require);
+    console.log(module);
+    console.log(exports);
+    console.log(module.exports);
+
+
+
+
+
+
+
---
+++ 09_Npm_modules/app.js
@@ -0,0 +1,12 @@
+
+const {num}=require('maths');
+
+//import {num} from 'maths'
+
+console.log(num)
+
+
+// what is npm modules
+
+// those modules which are prsent  inside node_modules folder are called npm modules
+ 
